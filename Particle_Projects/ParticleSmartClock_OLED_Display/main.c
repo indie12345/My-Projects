efd26daf5e04ea71d04e28d81d5784502afd5610
indie12345 <rahulrajsharma123@gmail.com>
2020-06-13 15:01:13 +0530

    Updated the main.c
    Made multiple changed to make the code more efficient and small.

--- Particle_Projects/ParticleSmartClock_OLED_Display/main.c
+++ Particle_Projects/ParticleSmartClock_OLED_Display/main.c
@@ -7,29 +7,21 @@ Thanks to Adafruit for their GFX and SSD1306 libraries
 
 Adafruit_SSD1306 display(-1);
 
-#define FONT_SIZE           5
+#define FONT_SIZE           4
 #define EEPROM_ADDR         0x00
 #define AM_PM_FORMAT        0x00
 #define HR_FORMAT           0x01
 #define DISPLAY_I2C_ADDR    0x3C
 
 bool flag = FALSE;
-int timeHour, timeMin, timeSec;
+int timeHour, timeMin, timeSec, timeNow, timeInPreviousReading, timeFormat;
 
 /* These must be of int type if we want to use Particle.variable function */
 int backupVariable;
 
-void updateTime(void);
-void updateDisplay(void);
-void doEverySecond(void);
 int watchMode(String cmd);
 void setTimeZoneIndia(void);
-void getTime(uint8_t timeFormat);
 void setTimeZoneGlobal(uint8_t value);
-
-/* Timer for 1 second interrupt, it will call 
-    "doEverySecond" function every second */
-Timer timer(1000, doEverySecond);
 
 void setup()   
 {   
@@ -42,8 +34,7 @@ void setup()
     /* Expose function to set 12h or 24H mode from the APP or Web Console */
     Particle.function("WatchMode", watchMode);
     
-    /* Start the timer and initialize the display */
-    timer.start();
+    /* Initialize the display */
     display.begin(SSD1306_SWITCHCAPVCC, DISPLAY_I2C_ADDR);
     
     /* Get data from the EEPROM and load into the structure */
@@ -58,12 +49,6 @@ void setup()
     
     /* Set the time zone of India */
     setTimeZoneIndia();
-    
-    /* Get the time in 12H or 24H format */
-	getTime(backupVariable);
-	
-	/* Update the display with the time */
-	updateDisplay();
 	
 	/* Disable the RGB LED's default function and control manually */
 	RGB.control(true);
@@ -72,18 +57,33 @@ void setup()
 
 void loop()
 {
-    /* Update the time if flag is set */
-    if(flag)
+    timeNow = Time.now();
+    
+    if(timeNow != timeInPreviousReading)
     {
-        flag = false;
-        updateTime();
+        timeInPreviousReading = timeNow;
+        timeSec = Time.second(timeNow);
+        
+        /* Draw lines to indicate seconds */
+        display.drawLine(4, 1, ((timeSec << 1) + 4), 1, WHITE);
+        display.drawLine(4, 3, ((timeSec << 1) + 4), 3, WHITE);
+        display.drawLine((124 - (timeSec << 1)), 61, 123, 61, WHITE);
+        display.drawLine((124 - (timeSec << 1)), 63, 123, 63, WHITE);
+        display.display();
+        
     }
-}
-
-/* Timer interrupt handler */
-void doEverySecond(void)
-{
-    flag = true;    
+    
+    if(timeMin != Time.minute(timeNow))
+    {
+        timeMin = Time.minute(timeNow);
+        timeHour = Time.hour(timeNow);
+        display.clearDisplay();
+        display.setTextSize(FONT_SIZE);
+        display.setTextColor(WHITE);
+        display.setCursor(0, 15);
+        display.println(Time.format("%H:%M"));
+        display.display();
+    }
 }
 
 /* Set to Indian time zone */
@@ -99,129 +99,21 @@ void setTimeZoneGlobal(uint8_t value)
      Time.zone(value);
 }
 
-/* Get time from the servers in specific format */
-void getTime(uint8_t timeFormat)
-{
-    int now = Time.now();
-    
-    if(timeFormat)
-    {
-        /* Time in 24H format */
-        timeHour = Time.hour(now);
-    }
-    
-    else
-    {
-        /* Time in 12H format */
-        timeHour = Time.hourFormat12(now);
-    }
-    
-    timeMin = Time.minute(now);
-    timeSec = Time.second(now);
-}
-
-/* Update the display */
-void updateDisplay(void)
-{
-    char str[8];
-    
-    display.clearDisplay();
-    display.setTextSize(FONT_SIZE);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 15);
-    
-    snprintf(str, sizeof(str), "%02d", timeHour);
-    display.println(str);
-    
-    display.setCursor(67, 15);
-    
-    snprintf(str, sizeof(str), "%02d", timeMin);
-    display.println(str);
-    
-    /* Print the colon between minute and hour */
-    display.setTextSize(FONT_SIZE - 1);
-    display.setCursor(52, 20);
-    display.println(":");
-}
-
-void updateTime(void)
-{
-    uint8_t tempTimeSec = 0;
-    
-    /* Increment second */
-    timeSec++;
-    
-    /* Make seconds '0' when it reaches 60, increase the minute */
-    if(timeSec > 59)
-    {
-        timeSec = 0;
-        timeMin++;
-    }
-    
-    /* Make minute '0' when it reaches 60 and increase the hour */
-    if(timeMin > 59)
-    {
-        timeMin = 0;
-        timeHour++;
-    }
-    
-    /* Update the hour, depending on the time format */
-    if(backupVariable == HR_FORMAT)
-    {
-        if(timeHour == 24)
-        {
-            timeHour = 0;
-        }
-    }
-    
-    else
-    {
-        if(timeHour == 13)
-        {
-            timeHour = 1;
-        }
-    }
-    
-    /* When second is '30' that is midway the minute then update the time if 
-    internal time is off by more than 2 seconds. We are doing this here as if 
-    we do when second is '0' then there might be cases where the used will see 
-    the time going back by a minute if the internal time is ahead of actual time.*/
-    if(timeSec == 30)
-    {
-        tempTimeSec = Time.second();
-        if((tempTimeSec > 32) || (tempTimeSec < 28))
-        {
-            getTime(backupVariable);
-        }
-    }
-    
-    /* Update the time whenever seconds is '0' */
-    if(!timeSec)
-    {
-        updateDisplay();
-    }
-    
-    /* Draw lines to indicate seconds */
-    display.drawLine(4, 1, ((timeSec << 1) + 4), 1, WHITE);
-    display.drawLine(4, 3, ((timeSec << 1) + 4), 3, WHITE);
-    display.drawLine((124 - (timeSec << 1)), 61, 123, 61, WHITE);
-    display.drawLine((124 - (timeSec << 1)), 63, 123, 63, WHITE);
-    display.display();
-}
-
 /* This function is called when the time mode 
     is change and the function is called */
 int watchMode(String cmd)
 {
+    /* Fill timeMin with some unexpected value to that 
+    doesn't match with the value from Time.minute function 
+    and therefore get's updated and printed on the display */
+    timeMin = 0xFF;    
+    
     if((cmd == "12H") || (cmd == "12h") || (cmd == "12"))
     {
         /* Update the variable and store it in EEPROM */
         backupVariable = AM_PM_FORMAT;
         EEPROM.put(EEPROM_ADDR, backupVariable);
         
-        /* Get the time in new format and update it on display */
-        getTime(AM_PM_FORMAT);
-        updateDisplay();
         return 12;
     }
     
@@ -229,8 +121,7 @@ int watchMode(String cmd)
     {
         backupVariable = HR_FORMAT;
         EEPROM.put(EEPROM_ADDR, backupVariable);
-        getTime(HR_FORMAT);
-        updateDisplay();
+        
         return 24;
     }
     
